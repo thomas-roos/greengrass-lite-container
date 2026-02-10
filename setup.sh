c347634ac0f1addeeac6fe92d6956018e57e6c53
Thomas Roos <throos@amazon.de>
2026-02-10 10:14:13 +0000

    Add persistent systemd unit directory and remove /etc tmpfs
    - Mount ./volumes/etc-systemd to /etc/systemd/system for persistent units
    - Remove /etc tmpfs mount to allow systemd customization
    - Create etc-systemd directory in setup.sh

--- setup.sh
+++ setup.sh
@@ -25,6 +25,7 @@ echo "Volume base: $VOLUME_BASE"
 mkdir -p "$VOLUME_BASE/etc-greengrass/config.d"
 mkdir -p "$VOLUME_BASE/var-lib-greengrass"
 mkdir -p "$VOLUME_BASE/var-lib-containers"
+mkdir -p "$VOLUME_BASE/etc-systemd"
 
 # Extract greengrass-lite.yaml from image if not already present
 if [ ! -f "$VOLUME_BASE/etc-greengrass/config.d/greengrass-lite.yaml" ]; then
